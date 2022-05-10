7fe11b09df62f61ffb9b0a65fa8779a1e094df22
RadhiyaJabed <62266767+RadhiyaJabed@users.noreply.github.com>
2022-05-10 20:41:22 +0100

    created a function to output the information from the API

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="+ city +",GB&units=metric&appid="+this.apiKey)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
     }
-}
+};
