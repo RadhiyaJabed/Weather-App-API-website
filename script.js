69dd46394da28be967b0ecd1f71731d47c81746a
RadhiyaJabed <62266767+RadhiyaJabed@users.noreply.github.com>
2022-05-10 23:31:58 +0100

    Used the navigator and geolocation API to find the location of the user and automatically give the users weather
    I used the geolocation to do a reverse geocode. I show an alert if the permissions aren't given.
    I've defined a couple of functions within another function which probably wasn't the best idea. I should tidy that.

--- script.js
+++ script.js
@@ -25,6 +25,82 @@ let weather = {
     }
 };
 
+let geocode = {
+    reverseGeocode: function(latitude,longitude){
+        var api_key = 'f0f88ead002a47dd9079778939266c43';
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+      
+        var request_url = api_url
+          + '?'
+          + 'key=' + api_key
+          + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+          + '&pretty=1'
+          + '&no_annotations=1';
+      
+        // see full list of required and optional parameters:
+        // https://opencagedata.com/api#forward
+      
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+      
+        request.onload = function() {
+          // see full list of possible response codes:
+          // https://opencagedata.com/api#codes
+      
+          if (request.status === 200){
+            // Success!
+            var data = JSON.parse(request.responseText);
+            weather.fetchWeather(data.results[0].components.city);
+          } else if (request.status <= 500){
+            // We reached our target server, but it returned an error
+      
+            console.log("unable to geocode! Response code: " + request.status);
+            var data = JSON.parse(request.responseText);
+            console.log('error msg: ' + data.status.message);
+          } else {
+            console.log("server error");
+          }
+        };
+      
+        request.onerror = function() {
+          // There was a connection error of some sort
+          console.log("unable to connect to server");
+        };
+      
+        request.send();  // make the request
+    },
+    getLocation : function (){
+        function success (data) {
+            geocode.reverseGeocode(data.coords.latitude,data.coords.longitude);
+        }
+        function showError(error) {
+            switch(error.code) {
+              case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.\nDefault location weather will be shown");
+                weather.fetchWeather("London");  
+                break;
+              case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.\nDefault location weather will be shown");
+                weather.fetchWeather("London");
+                break;
+              case error.TIMEOUT:
+                alert("The request to get user location timed out.\nDefault location weather will be shown");
+                weather.fetchWeather("London");
+                break;
+              case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.\nDefault location weather will be shown");
+                weather.fetchWeather("London");
+                break;
+            }
+          }
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success, showError);
+        }else {
+            weather.fetchWeather("London");
+        }
+    }
+}
+
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
@@ -34,4 +110,5 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
         weather.search();
     }
 });
-weather.fetchWeather("London");
+
+geocode.getLocation();
