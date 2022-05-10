29a4b5aed1f1375861cdd4b3b700c8496fecfaef
RadhiyaJabed <62266767+RadhiyaJabed@users.noreply.github.com>
2022-05-10 15:34:45 +0100

    prints data about any city in the uk to the console!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let weather = {
     "apiKey" : '9bcc314642d85159fb8593a1c4b9335b',
-    fetchWeather: function (){
+    fetchWeather: function (city){
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Lancaster,GB&units=metric&appid=9bcc314642d85159fb8593a1c4b9335b"
-            ).then((response) => response.json())
+            "https://api.openweathermap.org/data/2.5/weather?q="+ city +",GB&units=metric&appid="+this.apiKey)
+            .then((response) => response.json())
             .then((data) => console.log(data));
     }
 }
